24bb6e26902fa4aa5d5503577e4f71600202275b
xujun <debugeye@gmail.com>
2017-04-06 14:59:45 +0800

    实现板上4个可编程LED灯的闪烁

--- bsp/xmonkey/applications/main.c
+++ bsp/xmonkey/applications/main.c
@@ -3,11 +3,24 @@
 #include "string.h"
 #include "pinx.h"
 
+#define LED0_PIN PB4
+#define LED1_PIN PB2
+#define LED2_PIN PB3
+#define LED3_PIN PB11
+
+#define LED_COUNT  4
+static const rt_uint16_t pins[LED_COUNT] = {
+    LED0_PIN, LED1_PIN, LED2_PIN, LED3_PIN 
+};
 
 #define LED_PIN PC13
 static rt_err_t prolog(void)
 {
-    rt_pin_mode(LED_PIN, PIN_MODE_OUT_PP);
+    int i = 0;
+    for (i = 0; i < LED_COUNT; ++i)
+    {
+        rt_pin_mode(pins[i], PIN_MODE_OUT_PP);
+    }
     return RT_EOK;
 }
 static rt_err_t epilog(void)
@@ -17,18 +30,22 @@ static rt_err_t epilog(void)
 static void main_run(void* parameter)
 {
     rt_uint8_t onoff = 0;
+    int i;
     RT_ASSERT(prolog() == RT_EOK);
     while(1)
     {
         onoff = onoff?0:1;
-        rt_pin_write(LED_PIN, onoff);
+        for (i = 0; i < LED_COUNT; ++i)
+        {
+            rt_pin_write(pins[i], onoff);
+        }
         rt_thread_delay(50);
     }
     RT_ASSERT(epilog() == RT_EOK);
 }
 
 ALIGN(RT_ALIGN_SIZE)
-static rt_uint8_t       thread_stack[ 128 ];
+static rt_uint8_t       thread_stack[ 256 ];
 static struct rt_thread thread_handle;
 
 rt_err_t start_thread_led(void)
